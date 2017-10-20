a1f1254bbdc89cb726e0a1dd78e8dcbe5689f472
Heather Lansdell <spencerhb@vcu.edu>
2017-10-20 15:21:45 -0400

    Create Upper_Quartile_Normalization.R

---
+++ Upper_Quartile_Normalization.R
@@ -0,0 +1,37 @@
+setwd("C:/Users/hslansd/Desktop")
+
+library(factoextra)
+library(FactoMineR)
+library(caret)
+library(plyr)
+
+1RSEMreads<-read.csv("rsem_raw_batch1.csv", header=TRUE, row.names = 1, stringsAsFactors = FALSE)
+2RSEMreads<-read.csv("rsem_raw_batch2.csv", header=TRUE, row.names = 1, stringsAsFactors = FALSE)
+
+data<-as.matrix(rbind(t(1RSEMreads),t(2RSEMreads)))
+
+rownames(data) <- substring(rownames(data), 2)
+
+data<-data[,colSums(data)>0]
+
+data<-t(data)
+data<-as.data.frame(data)
+
+seventyfive_quart<-numcolwise(function(x) quantile(x, .75))(data)
+
+
+for(i in 1:192){data[,i]<-data[,i]/seventyfive_quart[,i]}  #192 number of columns
+
+
+mean_quart<-sum(seventyfive_quart)/192
+
+for(i in 1:192){data[,i]<-data[,i]*mean_quart}
+
+data<-t(data)
+
+#outliers from this c("10007","6584","3454")
+
+Ext_list=c("10007","6584","3454")
+
+data<-data[!rownames(data) %in% Ext_list,]
+fviz_pca_ind(prcomp(data))
